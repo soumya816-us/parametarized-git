62bee30ca82792251b69628508cef1cfe76a404b
soumya816-us <soumyareddy816@gmail.com>
2024-07-22 23:16:39 -0400

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+env = $1
+tag = $2
+
+echo "Environment : $env"
+echo "Tags : $tag"
+
+if["env" == "QA"]; then
+   echo " performing $env environments with $tag "
+
+else 
+  echo "select the correct environments"
+fi
+
+  if["tag" == "start"]; then
+     echo " start app "
+  else
+      echo " stop app "
+  fi
