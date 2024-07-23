160ad85d120d9e5aff10e52d47c3afbb04b907cf
soumya816-us <soumyareddy816@gmail.com>
2024-07-22 23:34:34 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,14 +5,14 @@ tag = $2
 echo "Environment : $env"
 echo "Tags : $tag"
 
-if["env" == "QA"]; then
+if["$env" == "QA"]; then
    echo " performing $env environments with $tag "
 
 else 
   echo "select the correct environments"
 fi
 
-  if["tag" == "start"]; then
+  if["$tag" == "start"]; then
      echo " start app "
   else
       echo " stop app "
